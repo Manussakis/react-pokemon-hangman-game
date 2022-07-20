a7139772400ab28e6a7569667cd37e1c70100b1e
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2022-07-20 18:40:07 -0300

    Create AttemptsDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Keyboard } from './components/Keyboard';
 import { WordInProgress } from './components/WordInProgress';
 import { Illustration } from './components/Illustration';
 import { Avatar } from './components/Avatar';
-import { RemainingAttemptsDisplay } from './components/RemainingAttemptsDisplay';
+import { AttemptsDisplay } from './components/AttemptsDisplay';
 
 import { useAppContext } from './context/AppContext';
 
@@ -14,9 +14,9 @@ function App() {
     <>
       <main>
         <div className="container">
+          <AttemptsDisplay remainingAttempts={remainingAttempts} />
           <Avatar name={name} image={image} />
           <Illustration remainingAttempts={remainingAttempts} />
-          <RemainingAttemptsDisplay remainingAttempts={remainingAttempts} />
           <WordInProgress wordInProgress={wordInProgress} />
           <Keyboard />
           <button onClick={onFindNewPokemon}>Find new Pokémon</button>
---
+++ src/components/AttemptsDisplay/index.tsx
@@ -0,0 +1,36 @@
+import { MAX_ATTEMPTS } from '../../library/constants';
+import { ReactComponent as Pokeball } from '../../assets/pokeball.svg';
+import { ReactComponent as PokeballLight } from '../../assets/pokeball-light.svg';
+import './style.scss';
+
+interface AttemptsDisplayProps {
+  remainingAttempts: number;
+}
+
+export const AttemptsDisplay = ({ remainingAttempts }: AttemptsDisplayProps) => {
+  const allPokeballs = Array.from(Array(MAX_ATTEMPTS).keys());
+
+  console.log(allPokeballs);
+
+
+  return (
+    <div className="attempts-display">
+      <div className="attempts-display__text">
+        Attempts - <span className="attempts-display__number">{`${remainingAttempts}/${MAX_ATTEMPTS}`}</span>
+      </div>
+      <ul className="attempts-display__list">
+        {allPokeballs.map((key) => {
+          const icon = (key + 1) <= remainingAttempts ?
+            <Pokeball className="attempts-display__icon" /> :
+            <PokeballLight className="attempts-display__icon" />;
+
+          return (
+            <li className="attempts-display__item">
+              {icon}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  )
+}
