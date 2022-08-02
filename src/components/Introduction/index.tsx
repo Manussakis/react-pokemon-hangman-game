e66daf3359ebceadbb4f455da1dbc5074786a832
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2022-08-02 13:46:35 -0300

    Add styled component to Introduction comp

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,24 +1,22 @@
-import { ReactComponent as Pokeball } from '../../assets/pokeball.svg';
+import { useAppContext } from '../../contexts/AppContext';
 import { Button } from '../Button';
 import { ButtonTypeEnum } from '../Button/enums';
 
-import { useAppContext } from '../../contexts/AppContext';
-
-import './style.scss';
+import { StyledWrapper, StyledPokeball, StyledContent } from './styles';
 
 export const Introduction = () => {
   const { onStartGame } = useAppContext();
 
   return (
-    <div className="introduction text-center">
-      <Pokeball className="introduction__icon" />
+    <StyledWrapper>
+      <StyledPokeball />
       <h1>Can you spell any Pokémon name?</h1>
-      <div className="introduction__content">
+      <StyledContent>
         <p>This is a Pokémon version of the hangman game.</p>
         <p>A random Pokémon will appear, can you spell its name correctly? You'll have 6 attemps to do that.</p>
         <p className="font-bold">Good luck!</p>
-      </div>
+      </StyledContent>
       <Button type={ButtonTypeEnum.PRIMARY} onClick={() => onStartGame()}>Start</Button>
-    </div>
+    </StyledWrapper>
   )
 }
---
+++ src/components/Introduction/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { ReactComponent as Pokeball } from '../../assets/pokeball.svg';
+
+export const StyledWrapper =  styled.div`
+  padding-top: 2rem;
+  padding-bottom: 3rem;
+  margin: 0 auto;
+  max-width: 400px;
+  text-align: center;
+
+  @media screen and (min-width: 480px) {
+    padding-top: 5rem;
+    padding-bottom: 5rem;
+  }
+`;
+
+export const StyledPokeball =  styled(Pokeball)`
+  position: relative;
+  left: -10px;
+  width: 100px;
+  height: 100px;
+`;
+
+export const StyledContent =  styled.div`
+  margin-bottom: 2rem;
+`;
