774bd3df6d7748f1099ba3b588eb04c70679a0ca
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2022-08-03 11:25:44 -0300

    Test App renders footer links

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { AppContextProvider } from './contexts/AppContext';
 
-test('renders author link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Gabriel Manussakis/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  render(<AppContextProvider><App /></AppContextProvider>);
+
+  test('renders footer links', () => {
+    const authorLink = screen.getByText('Gabriel Manussakis');
+    const githubLink = screen.getByRole('link', { name: /Github/i });
+
+    expect(authorLink).toHaveAttribute('href', 'https://github.com/manussakis');
+    expect(githubLink).toHaveAttribute('href', 'https://github.com/manussakis/react-pokemon-hangman-game');
+  });
+})
+
+
