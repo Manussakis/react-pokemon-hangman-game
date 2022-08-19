803b999302d6275a6703c6e9ae06e3331aec25fd
Gabriel Manussakis <Manussakis@users.noreply.github.com>
2022-08-19 16:08:13 -0300

    Test start game in App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
 import { AppContextProvider } from './contexts/AppContext';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+const startButtonText = 'Start';
+const loadNewPokemonButtonText = 'Load new Pokémon';
+
+const renderApp = () => render(<AppContextProvider><App /></AppContextProvider>);
 
 describe('App', () => {
-  render(<AppContextProvider><App /></AppContextProvider>);
+  test('renders introduction', () => {
+    renderApp();
+
+    const h1 = screen.getByRole('heading', { level: 1 });
+    const startButton = screen.getByRole('button', { name: startButtonText });
+    const loadNewPokemonButton = screen.queryByRole('button', { name: loadNewPokemonButtonText });
+
+    expect(h1).toBeInTheDocument();
+    expect(startButton).toBeInTheDocument();
+    expect(loadNewPokemonButton).toBeNull();
+  });
+
+  test('starts game when clicking on the "Start" button', ()=> {
+    renderApp();
+
+    const startButton = screen.getByRole('button', { name: startButtonText });
+
+    userEvent.click(startButton);
+
+    const loadNewPokemonButton = screen.getByRole('button', { name: loadNewPokemonButtonText });
+
+    expect(startButton).not.toBeInTheDocument();
+    expect(loadNewPokemonButton).toBeInTheDocument();
+  });
 
   test('renders footer links', () => {
+    renderApp();
+
     const authorLink = screen.getByText('Gabriel Manussakis');
     const githubLink = screen.getByRole('link', { name: /Github/i });
 
